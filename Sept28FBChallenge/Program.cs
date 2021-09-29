076741b3058ef29fc0956e8ea9d1344be513dc34
Rex Meyer <armeyer1998@gmail.com>
2021-09-29 01:15:01 -0400

    EFA Facebook Challenge

---
+++ Sept28FBChallenge/Program.cs
@@ -0,0 +1,23 @@
+﻿using Sept28FBClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sept28FBChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile facebookProfile = new Profile("Rex", "Meyer", new DateTime(1998, 03, 04), Relationship.Single, "Front Office Supervisor");
+
+            Console.WriteLine(facebookProfile.FullName);
+            Console.WriteLine(facebookProfile.Age);
+            Console.WriteLine(facebookProfile.Status);
+            Console.WriteLine(facebookProfile.Occupation);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sept28FBClass/Facebook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sept28FBClass
+{
+    public enum Relationship { Single, Married, Taken, Complicated }
+    public class Profile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+        public DateTime BirthDate { get; set; }
+        public int Age
+        {
+            get
+            {
+                TimeSpan profileAge = DateTime.Now - BirthDate;
+                double totalAgeYears = profileAge.TotalDays / 365.25;
+                return Convert.ToInt32(Math.Floor(totalAgeYears));
+            }
+        }
+        public Relationship Status { get; set; }
+        public string Occupation { get; set; }
+
+        public Profile(string firstName, string lastName, DateTime dateOfBirth, Relationship status, string occupation)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = dateOfBirth;
+            Status = status;
+            Occupation = occupation;
+        }
+        public Profile()
+        {
+
+        }
+    }
+}
